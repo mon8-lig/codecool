9d52171e9927555da684fa59c3825f3afc6a152a
monika.zdun <monika.zdun@motorolasolutions.com>
2018-04-14 11:47:13 +0200

    codecool

---
+++ codecoolkrakow/HttpOrderFormSave.cs
@@ -0,0 +1,30 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace codecoolkrakow
+{
+    public static class HttpOrderFormSave
+    {
+        [FunctionName("HttpOrderFormSave")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            PhotoOrder orderData = null;
+            try
+            {
+                string requestBody = new StreamReader(req.Body).ReadToEnd();
+                orderData = JsonConvert.DeserializeObject<PhotoOrder>(requestBody);
+            }
+            catch (System.Exception)
+            {
+                return new BadRequestObjectResult("Received data invalid");
+            }
+            return (ActionResult)new OkObjectResult($"Order processed");
+        }
+    }
+}
