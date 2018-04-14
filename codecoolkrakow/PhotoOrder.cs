64dff1b00998409c48c7feb3c6094c85f4729372
monika.zdun <monika.zdun@motorolasolutions.com>
2018-04-14 14:06:59 +0200

    commit 6

--- codecoolkrakow/PhotoOrder.cs
+++ codecoolkrakow/PhotoOrder.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace codecoolkrakow
 {
-    public class PhotoOrder
+    public class PhotoOrder : TableEntity
     {
-        public string CustomerEmail {get; set; }
-        public string FileName{ get; set; }
+        public string CustomerEmail { get; set; }
+        public string FileName { get; set; }
         public int RequiredHeight { get; set; }
         public int RequiredWidth { get; set; }
     }
