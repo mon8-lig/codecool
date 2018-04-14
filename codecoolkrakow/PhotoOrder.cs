18cf35021c022d3dff7defccff577c3ddc9ebd67
monika.zdun <monika.zdun@motorolasolutions.com>
2018-04-14 11:56:10 +0200

    initial commit

---
+++ codecoolkrakow/PhotoOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codecoolkrakow
+{
+    public class PhotoOrder
+    {
+        public string CustomerEmail {get; set; }
+        public string FileName{ get; set; }
+        public int RequiredHeight { get; set; }
+        public int RequiredWidth { get; set; }
+    }
+}
