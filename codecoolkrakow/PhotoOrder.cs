fe0f08c08bd3b8bf3769dcde84d7500172cdb0b6
mon8-lig <mon8-lig@o2.pl>
2018-05-10 07:33:59 +0200

    Added files

--- codecoolkrakow/PhotoOrder.cs
+++ codecoolkrakow/PhotoOrder.cs
@@ -9,7 +9,6 @@ namespace codecoolkrakow
     {
         public string CustomerEmail { get; set; }
         public string FileName { get; set; }
-        public int RequiredHeight { get; set; }
-        public int RequiredWidth { get; set; }
+        public string Resolutions { get; set; }
     }
 }
