f5dbf04c01e283a701b2a8b71024a702fb82e272
mon8-lig <mon8-lig@o2.pl>
2018-05-19 17:11:49 +0200

    durable function

---
+++ codecoolkrakow/HttpDurableGetSASForBlob.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.IO;
+using System;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace codecoolkrakow
+{
+    public static class HttpDurableGetSASForBlob
+    {
+        [FunctionName("HttpDurableGetSASForBlob")]
+        public static async Task<string> RunOrchestrator(
+            [OrchestrationTrigger] DurableOrchestrationContext context)
+        {
+            var resizedPicturesNames = context.GetInput<List<string>>();
+
+            var tasks = new Task<string>[resizedPicturesNames.Count];
+            for (int i = 0; i < resizedPicturesNames.Count; i++)
+            {
+                tasks[i] = context.CallActivityAsync<string>(
+                "HttpDurableGetSASForBlob_GetSAS",
+                resizedPicturesNames[i]);
+            }
+
+            await Task.WhenAll(tasks);
+
+            string uri = "";
+            foreach (var task in tasks)
+            {
+                uri += task.Result + " ";
+            }
+            return uri;
+        }
+
+        [FunctionName("HttpDurableGetSASForBlob_GetSAS")]
+        public static async Task<string> HttpGetSharedAccessSignatureForBlobAsync([ActivityTrigger] string fileName,
+            [Blob("doneorders", FileAccess.Read, Connection = "StorageConnection")]CloudBlobContainer photosContainer, TraceWriter log)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+                return String.Empty;
+
+            var photoBlob = await photosContainer.GetBlobReferenceFromServerAsync(fileName);
+            return GetBlobSasUri(photoBlob);
+        }
+
+        [FunctionName("HttpDurableGetSASForBlob_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")]HttpRequestMessage req,
+            [OrchestrationClient]DurableOrchestrationClient starter,
+            TraceWriter log)
+        {
+            var content = req.Content;
+            string jsonContent = content.ReadAsStringAsync().Result;
+            dynamic resizedPicturesNames = JsonConvert.DeserializeObject<List<string>>(jsonContent);
+
+            string instanceId = await starter.StartNewAsync("HttpDurableGetSASForBlob", resizedPicturesNames);
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+
+        static string GetBlobSasUri(ICloudBlob cloudBlob)
+        {
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessStartTime = DateTimeOffset.UtcNow.AddHours(-1);
+            sasConstraints.SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddHours(24);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.List | SharedAccessBlobPermissions.Read;
+
+            string sasToken = cloudBlob.GetSharedAccessSignature(sasConstraints);
+
+            return cloudBlob.Uri + sasToken;
+        }
+    }
+}
